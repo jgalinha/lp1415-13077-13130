c942ba959a265a7fa31a13a9c56859af767ba080
jgalinha <j.b.galinha@gmail.com>
2014-10-07 00:31:13 +0100

    Class excel read the sheet 'Dados' to a list of lists

--- src/excel.py
+++ src/excel.py
@@ -3,11 +3,14 @@
 # data: 05 de outubro de 2014
 #
 
-from mmap import mmap,ACCESS_READ
+from mmap import mmap, ACCESS_READ
 from xlrd import open_workbook
+
 
 class Excel:
     excelFileLoc = ""
+    workBook = ""
+    lista = [[[]]]
 
     def __init__(self, excelfilelocation):
         Excel.excelFileLoc = excelfilelocation
@@ -15,7 +18,15 @@ class Excel:
 
     def readFile(self):
 
-        with open(Excel.excelFileLoc, 'rb') as f:
-            print open_workbook(
-                file_contents=mmap(f.fileno(),0,access=ACCESS_READ)
-                )
+        Excel.workBook = open_workbook(Excel.excelFileLoc)
+        x = 0
+        y = 0
+        for s in Excel.workBook.sheets():
+            print 'Sheet:', s.name
+            if x == 0:
+                for row in range(s.nrows):
+                    Excel.lista[x].append([])
+                    for col in range(s.ncols):
+                        Excel.lista[x][y].append(s.cell(row, col).value)
+                    y += 1
+            x += 1
